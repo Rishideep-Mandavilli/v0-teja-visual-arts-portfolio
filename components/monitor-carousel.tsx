5e494bb76ada47cc1b23d6ebbdd7ca849b2fe91a
Rishideep_Mandavilli <rishideep15@gmail.com>
2026-05-13 15:36:39 +0530

    Update monitor-carousel.tsx

--- components/monitor-carousel.tsx
+++ components/monitor-carousel.tsx
@@ -22,7 +22,7 @@ const projects = [
   },
   {
     id: 3,
-    title: "GVP Sankranthi Celebrations",
+    title: "GVP Fest Celebrations",
     category: "Trailer Video",
     year: "2025",
     thumb: "/images/gvp-college.mp4",
